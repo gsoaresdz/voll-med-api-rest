0e63742eae4edb51de5cdf94f9c09856204bfbbf
Gabriel Soares <gsoaresdesouza@live.com>
2023-10-12 21:02:07 -0300

    DTO com Java Record

---
+++ api/src/main/java/med/voll/api/medico/endereco/DadosEndereco.java
@@ -0,0 +1,6 @@
+package med.voll.api.medico.endereco;
+
+public record DadosEndereco(String logradouro, String bairro, String cep, String cidade, String uf, String numero,
+    String complemento) {
+
+}
