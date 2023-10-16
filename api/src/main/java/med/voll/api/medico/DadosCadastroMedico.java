19f137075e19a33e80bd2f67fa737b99b930fb80
Gabriel Soares <gsoaresdesouza@live.com>
2023-10-16 08:06:40 -0300

    Nova migration

--- api/src/main/java/med/voll/api/medico/DadosCadastroMedico.java
+++ api/src/main/java/med/voll/api/medico/DadosCadastroMedico.java
@@ -8,10 +8,11 @@ import jakarta.validation.constraints.Pattern;
 import med.voll.api.medico.endereco.DadosEndereco;
 
 public record DadosCadastroMedico(
-        @NotBlank String nome,
-        @NotBlank @Email String email,
-        @NotBlank @Pattern(regexp = "\\d{4,6}") String crm,
-        @NotNull Especialidade especialidade,
-        @NotNull @Valid DadosEndereco endereco) {
+                @NotBlank String nome,
+                @NotBlank @Email String email,
+                @NotBlank String telefone,
+                @NotBlank @Pattern(regexp = "\\d{4,6}") String crm,
+                @NotNull Especialidade especialidade,
+                @NotNull @Valid DadosEndereco endereco) {
 
 }
