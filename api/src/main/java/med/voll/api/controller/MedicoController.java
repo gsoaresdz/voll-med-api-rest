b97f3ab3cb13631274a24668ed7a93c0eb00bcf4
Gabriel Soares <gsoaresdesouza@live.com>
2023-10-19 20:18:54 -0300

    Atualizando dados

--- api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.validation.Valid;
+import med.voll.api.medico.DadosAtualizacaoMedico;
 import med.voll.api.medico.DadosCadastroMedico;
 import med.voll.api.medico.DadosListagemMedico;
 import med.voll.api.medico.Medico;
@@ -38,6 +39,8 @@ public class MedicoController {
 
   @PutMapping
   @Transactional
-  public void atualizar(@RequestBody @Valid DadosCadastroMedico dados) {
+  public void atualizar(@RequestBody @Valid DadosAtualizacaoMedico dados) {
+    var medico = repository.getReferenceById(dados.id());
+    medico.atualizarInformacoes(dados);
   }
 }
--- api/src/main/java/med/voll/api/medico/Medico.java
+++ api/src/main/java/med/voll/api/medico/Medico.java
@@ -45,4 +45,19 @@ public class Medico {
 
   }
 
+  public void atualizarInformacoes(DadosAtualizacaoMedico dados) {
+    if (dados.nome() != null) {
+      this.nome = dados.nome();
+    }
+
+    if (dados.telefone() != null) {
+      this.email = dados.telefone();
+    }
+
+    if (dados.endereco() != null)
+
+    {
+      this.endereco.atualizarInformacoes(dados.endereco());
+    }
+  }
 }
