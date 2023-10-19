e4e89842bd28ee7887c70df320495ad4a302ed8f
Gabriel Soares <gsoaresdesouza@live.com>
2023-10-19 20:04:16 -0300

    Requisições PUT

--- api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -7,6 +7,7 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,5 +34,10 @@ public class MedicoController {
   @GetMapping
   public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, page = 0, sort = { "nome" }) Pageable paginacao) {
     return repository.findAll(paginacao).map(DadosListagemMedico::new);
+  }
+
+  @PutMapping
+  @Transactional
+  public void atualizar(@RequestBody @Valid DadosCadastroMedico dados) {
   }
 }
