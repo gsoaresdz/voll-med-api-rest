2c1e172da217cf9287a51b0701444c479fbfacaf
Gabriel Soares <gsoaresdesouza@live.com>
2024-05-13 09:06:05 -0300

    Validações de integridade

--- api/src/main/java/med/voll/api/domain/consulta/AgendaDeConsultas.java
+++ api/src/main/java/med/voll/api/domain/consulta/AgendaDeConsultas.java
@@ -3,6 +3,7 @@ package med.voll.api.domain.consulta;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import med.voll.api.domain.medico.Medico;
 import med.voll.api.domain.medico.MedicoRepository;
 import med.voll.api.domain.paciente.PacienteRepository;
 
@@ -19,10 +20,21 @@ public class AgendaDeConsultas {
   private PacienteRepository pacienteRepository;
 
   public void agendar(DadosAgendamentoConsulta dados) {
+    if (!pacienteRepository.existsById(dados.idPaciente())) {
+      throw new ValidacaoException("Id do paciente informado não existe!");
+    }
+
+    if (dados.idMedico() != null && !medicoRepository.existsById(dados.idMedico())) {
+      throw new ValidacaoException("Id do medico informado não existe!");
+    }
 
     var paciente = pacienteRepository.findById(dados.idPaciente()).get();
-    var medico = medicoRepository.findById(dados.idMedico()).get();
+    var medico = escolherMedico(dados);
     var consulta = new Consulta(null, medico, paciente, dados.data());
     consultaRepository.save(consulta);
+  }
+
+  private Medico escolherMedico(DadosAgendamentoConsulta dados) {
+    
   }
 }
